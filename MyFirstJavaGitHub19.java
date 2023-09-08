5ebffdd56784b562de095ed92e32da6b7d6e5121
FirmanJK <jatikusuma761@gmail.com>
2023-09-08 21:40:27 +0700

    simpan kode java ke GitHub

---
+++ MyFirstJavaGitHub19.java
@@ -0,0 +1,10 @@
+public class MyFirstJavaGitHub19 {
+
+     public static void main(String[] args) {
+        System.out.println("Nama Saya Mochammad Firmandika Jati Kusuma");
+        System.out.println("Jurusan Teknologi Informasi");
+        System.out.println("Prodi D-IV Teknik Informatika");
+        
+    }
+    
+}
